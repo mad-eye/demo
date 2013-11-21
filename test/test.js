35e7ddb5bc8b22f85e2491e8a62c82fc1c8c1b49
rissem <rissem@gmail.com>
2013-11-20 16:15:44 -0800

    update tests

--- test/test.js
+++ test/test.js
@@ -1,12 +1,31 @@
+var File = require("../models/File");
 var assert = require("assert");
 describe('File', function(){
-  describe('parse', function(){
-    it('should be able to parse a simple JSON file', function(){
-      assert.equal(10, functions.length);
+  describe("exension", function(){
+    it("should compute the extension correctly for `package.json`", function(){
+      var file = new File("package.json");
+      assert.equal(file.extension(), ".json");
     });
 
-    it("third function should return the third element", function(){
-      assert.equal(elements[2],functions[2]());
+    it("should compute the extension correctly for windows paths", function(){
+      var file = new File("C:\\office\\config.ini");
+      assert.equal(file.extension(), ".ini");
+    });
+
+    it("should return an empty string  when a file does not have an extension", function(){
+      var file = new File("/tmp/README");
+      assert.equal(file.extension(), "");
+    });
+  });
+
+  describe('parse', function(){
+    it('should be able to read and parse a simple JSON file', function(done){
+      var file = new File("test/simple.json");
+      file.read(function(){
+        file.parse();
+        assert.equal(file.data.foo, "bar");
+        done();
+      });
     });
   });
 });
